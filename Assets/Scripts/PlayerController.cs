35caf32e60f731e7123b6bc2566d094f6584c6ef
Christian Teguim <cteguimtu620@gmail.com>
2024-11-19 17:06:15 -0500

    destroy powerup on contact

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerup = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,12 @@ public class PlayerController : MonoBehaviour
        float fowardInput = Input.GetAxis("Vertical");
 
        playerRb.AddForce(focalPoint.transform.forward * speed * fowardInput); 
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Powerup")) {
+            hasPowerup = false;
+            Destroy(other.gameObject);
+        }
     }
 }
