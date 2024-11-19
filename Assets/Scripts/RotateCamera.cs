1962bd5a520551ca975a2203ecbe2e85458df2a4
Christian Teguim <cteguimtu620@gmail.com>
2024-11-19 16:32:00 -0500

    change speed

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    private float rotationSpeed = 70;
+    public float rotationSpeed = 25;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class RotateCamera : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, -horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
