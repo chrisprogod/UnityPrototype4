54b662e390620499d41a4b4522c41911260b649e
Christian Teguim <cteguimtu620@gmail.com>
2024-11-24 15:49:30 -0500

    spaw Multiple enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    private float spawnRange = 9;
-    // Start is called before the first frame update
-    void Start()
-    {
-		Instantiate(enemyPrefab,GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-    }
+  public GameObject enemyPrefab;
+  private float spawnRange = 9;
+  // Start is called before the first frame update
+  void Start()
+  {
+    SpawnEnemyWave(3);
+  }
 
-    // Update is called once per frame
-    void Update()
+  void SpawnEnemyWave(int enemiesToSpawn)
+  {
+    for (int i = 0; i < enemiesToSpawn; i++)
     {
-        
+      Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
+  }
 
-    private Vector3 GenerateSpawnPosition()
-	{
-		float spawnPosX = Random.Range(-spawnRange, spawnRange);
-		float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-		Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-		return randomPos;
-	}
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private Vector3 GenerateSpawnPosition()
+  {
+    float spawnPosX = Random.Range(-spawnRange, spawnRange);
+    float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+    Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+    return randomPos;
+  }
 }
